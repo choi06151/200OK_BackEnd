541eb3e7b010e905812cf1b78d38779004408360
kimdonghyuk <kdh367036@naver.com>
2024-11-08 11:42:35 +0900

    FEAT : 유저정보 수정 로직 추가

--- src/main/java/com/_OK/_OK/User/UserController.java
+++ src/main/java/com/_OK/_OK/User/UserController.java
@@ -17,11 +17,21 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping
     @Operation(summary = "유저 생성", description = "유저를 생성합니다.<br> id는 자동생성됩니다. id, day 필드는 Body에 포함하지 마세요.")
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createUser = userService.createUser(userDto);
         return new ResponseEntity<>(createUser, HttpStatus.CREATED);
+    }
+    @GetMapping("/userInfo/{id}")
+    @Operation(summary = "플레이어 정보 조회",description = "플레이어의 정보를 조회합니다.<br>id는 유저 고유id를 의미한다.")
+    public ResponseEntity<UserDto> Userinfo(@PathVariable("id") Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        UserDto userDto = UserMapper.mapToUserDto(user);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
     }
 }
