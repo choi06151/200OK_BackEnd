35894ba57941b779bad3cd240fc06f0a89e36b36
kimdonghyuk <kdh367036@naver.com>
2024-11-13 00:31:36 +0900

    FEAT : 로딩 독백 API

--- src/main/java/com/_OK/_OK/Story/MonologueDto.java
+++ src/main/java/com/_OK/_OK/Story/MonologueDto.java
@@ -1,2 +1,16 @@
-package com._OK._OK.Story;public class MonologueDto {
+package com._OK._OK.Story;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MonologueDto {
+    private ArrayList<String> monologue; //멀티스레드가 없으니 ArrayList 씀
 }
