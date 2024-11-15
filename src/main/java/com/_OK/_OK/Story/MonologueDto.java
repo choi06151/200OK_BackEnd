dbda4da6db658eed39947fc2c723032eeec51f90
kimdonghyuk <kdh367036@naver.com>
2024-11-15 16:43:14 +0900

    FEAT : Monologue tag추가

--- src/main/java/com/_OK/_OK/Story/MonologueDto.java
+++ src/main/java/com/_OK/_OK/Story/MonologueDto.java
@@ -13,4 +13,5 @@ import java.util.ArrayList;
 @AllArgsConstructor
 public class MonologueDto {
     private ArrayList<String> monologue; //멀티스레드가 없으니 ArrayList 씀
+    private String tag; //대사에 맞는 브금 태그
 }
